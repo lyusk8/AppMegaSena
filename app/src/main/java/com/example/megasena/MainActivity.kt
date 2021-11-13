a14a03f06c2f9185a72e882e3550e066cc3565ef
lyusk8 <lyusk8@gmail.com>
2021-11-13 11:05:38 -0300

    implementado o sorteio dos números

--- app/src/main/java/com/example/megasena/MainActivity.kt
+++ app/src/main/java/com/example/megasena/MainActivity.kt
@@ -2,23 +2,31 @@ package com.example.megasena
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val botaoNumero = findViewById<Button>(R.id.button)
+        botaoNumero.setOnClickListener {
+            gerarAposta()
+        }
     }
 
-    fun gerarAposta(){
+    private fun gerarAposta(){
         val numeros = findViewById<TextView>(R.id.numerosSorteados)
-        numeros.text = sortearNumeros().toString()
+        val texto = sortearNumeros().toString()
+        numeros.setText(texto)
     }
 
     private fun sortearNumeros():ArrayList<Int>{
         val numeros = ArrayList<Int>()
         while(numeros.size <7){
-            val numero = Math.random().toInt()*60+1
+            val numero = ((1..60).random())
             if(numero in numeros){
                 continue
             }
